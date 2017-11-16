74e86e835b00d15ac90a1fd2e6717ca84a64ae83
Samuel Widén <samuel.widen@learnet.se>
2017-11-16 11:11:38 +0100

    Added index, login, and register.

--- app.rb
+++ app.rb
@@ -1,7 +1,24 @@
 class App < Sinatra::Base
 
+	enable:sessions 
+
 	get '/' do
-		"Hello, Grillkorv!"
+		slim(:index)
 	end
 
+	post '/login' do
+		
+	end
+
+	get '/register' do
+		slim(:register)
+	end
+
+	post '/create' do
+		db = SQLite3::Database.new("todoapp.sqlite")
+		enter_password = params[:password]
+		enter_username = params[:username]
+		db.execute("INSERT INTO Users (username, password) VALUES(?,?)", [enter_username, enter_password])
+		redirect '/'
+	end 
 end           
