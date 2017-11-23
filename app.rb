7c2bb8155e82133e6a56faf357fbd1d6ff65b4fc
Samuel Widén <samuel.widen@learnet.se>
2017-11-23 09:44:19 +0100

    BCrypt + login session (ej logout)

--- app.rb
+++ app.rb
@@ -1,24 +1,49 @@
 class App < Sinatra::Base
 
-	enable:sessions 
+	enable :sessions 
 
 	get '/' do
 		slim(:index)
 	end
 
 	post '/login' do
-		
+		db = SQLite3::Database.new("todoapp.sqlite")
+		user_name = params[:username]
+		password_digest = db.execute("SELECT password FROM Users WHERE username IS ?", user_name) #Errors out, what
+		password_digest = password_digest[0][0]
+		p password_digest
+		password_digest = BCrypt::Password.new(password_digest)
+		password = params[:password]
+		if password_digest == password
+			user_id = db.execute("SELECT id FROM Users WHERE username IS ?", user_name)
+			session[:logged_in] = true
+			session[:user_id] = user_id
+			redirect '/notes'
+		else
+			redirect '/login_error'
+		end
 	end
 
 	get '/register' do
 		slim(:register)
 	end
 
+	get('/registered') do
+		slim(:registered)
+	end
+
 	post '/create' do
 		db = SQLite3::Database.new("todoapp.sqlite")
-		enter_password = params[:password]
-		enter_username = params[:username]
-		db.execute("INSERT INTO Users (username, password) VALUES(?,?)", [enter_username, enter_password])
-		redirect '/'
+		password = params[:password]
+		password_digest = BCrypt::Password.create( password )
+		# p password_digest					#Felsökte databasens "BLOB" issue, bcrypt funkar som förväntat trots det. 
+		# if password_digest == "cool"
+		# 	p "Cool password"
+		# else
+		# 	p "uncool password bruh"
+		# end
+		username = params[:username]
+		db.execute("INSERT INTO Users (username, password) VALUES(?,?)", [username, password_digest])
+		redirect '/registered'
 	end 
 end           
